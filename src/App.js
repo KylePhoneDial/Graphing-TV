aa587df88f2297d307d5f2fa1659ab5f7cee0ae1
KylePhoneDial <KylePhoneDial@gmail.com>
2018-02-16 15:50:44 -0600

    SearchForm and eslint config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import SearchForm from './components/forms/SearchForm';
 
-const App = () => <div>HELLO</div>;
+const App = () => (
+	<div>
+		<h1>Graphing-TV</h1>
+		
+		<SearchForm />		
+	</div>
+
+);
 
 export default App;
---
+++ src/components/forms/SearchForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Form, Button } from "semantic-ui-react";
+
+class SearchForm extends React.Component {
+
+	state = {
+		query: "",
+		loading: false
+	};
+
+	onSubmit = (e, data) => {
+		e.preventDefault();
+		this.setState({query: data.value, loading: true});
+	};
+
+	onChange = (e, data) => {
+		this.setState({query: data.value, loading: true});
+
+		console.log("onChange: " + this.state.query + " | " + this.state.loading)
+	};
+
+	render () {
+		return (
+			<Form onSubmit={this.onSubmit}>
+				<Form.Input
+					name='query'
+					placeholder='Please enter a TV show'
+					onChange={this.onChange}
+				/>
+				<Button 
+					type='submit'
+					content='Search'
+				/>
+			</Form>
+		);
+	}
+}
+
+export default SearchForm;
